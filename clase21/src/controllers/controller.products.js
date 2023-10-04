00c8df29829deb5fed4ff7dd965fd44a77e255bd
asimov86 <kennyjosue8@gmail.com>
2023-10-04 20:52:46 -0300

    Actualización

--- clase21/src/controllers/controller.products.js
+++ clase21/src/controllers/controller.products.js
@@ -5,99 +5,6 @@ const Products = new ProductsDao();
 const router = Router();
 
 
-
-// Render
-
-/* router.get('/realTimeProducts', async (req, res) => {
-    // Agregando límite, si no se agrega el límite trae todo los productos, de traer el límite trae la cantidad indicada.
-    let limitValue = parseInt(req.query.limit, 10) || 10;
-    let page = parseInt(req.query.page, 10) || 1;
-    //console.log(page);
-    let customQuery = req.query.query;
-    if(!customQuery){
-        customQuery = '';
-    }else{
-        customQuery = customQuery.toLowerCase();
-    }
-    console.log(customQuery);
-    let sort = parseInt(req.query.sort) || '';
-    const products = await Products.findAll(customQuery,page,limitValue,sort);
-    const {docs,hasPrevPage,hasNextPage,nextPage,prevPage,totalPages,prevLink,nextLink} = products;
-    // Para la paginación
-    let arr = [];
-    for (let i = 0; i < totalPages; i++) {
-        arr[i]=i + 1;
-      }
-    res.render('realTimeProducts.handlebars', { 
-        newProduct: docs,
-        hasPrevPage:hasPrevPage,
-        hasNextPage:hasNextPage,
-        prevPage:prevPage,
-        nextPage:nextPage,
-        totalPages:totalPages,
-        prevLink:prevLink,
-        nextLink:nextLink,
-        limitValue:limitValue,
-        sort:sort,
-        customQuery:customQuery,
-        arr,
-        listExists: true,
-    });
-}); */
-
-/* router.get('/realTimeProducts', async (req, res) => {
-    const newProduct = await Products.findAll();
-    console.log(newProduct);
-    const stringifiedProducts = newProduct.map(product => ({
-        ...product.toObject(),
-        _id: product._id.toString()
-    }));
-    console.log(stringifiedProducts);
-    res.render('realTimeProducts.handlebars', { 
-        newProduct: stringifiedProducts,
-        listExists: true,});
-}); */
-
-/* router.get('/home', async (req, res) => {
-    // Agregando límite, si no se agrega el límite trae todo los productos, de traer el límite trae la cantidad indicada.
-    let limitValue = parseInt(req.query.limit, 10) || 10;
-    let page = parseInt(req.query.page, 10) || 1;
-    let customQuery = req.query.query;
-    if(!customQuery){
-        customQuery = '';
-    }else{
-        customQuery = customQuery.toLowerCase();
-    }
-    console.log(customQuery);
-    let sort = parseInt(req.query.sort) || 1;
-    const listProducts = await Products.findAll(customQuery,page,limitValue,sort);
-    const allProducts = listProducts.docs;
-    console.log(allProducts);
-    const stringifiedProducts = allProducts.map(product => ({
-        ...product,
-        _id: product._id.toString()
-    }));
-    // Para la paginación
-    let arr = [];
-    for (let i = 0; i < listProducts.totalPages; i++) {
-        arr[i]=i + 1;
-      }
-    res.render('home.handlebars', { 
-        listProducts: stringifiedProducts,
-        hasPrevPage:listProducts.hasPrevPage,
-        hasNextPage:listProducts.hasNextPage,
-        prevPage:listProducts.prevPage,
-        nextPage:listProducts.nextPage,
-        totalPages:listProducts.totalPages,
-        prevLink:listProducts.prevLink,
-        nextLink:listProducts.nextLink,
-        limitValue:listProducts.limitValue,
-        sort:listProducts.sort,
-        customQuery:listProducts.customQuery,
-        arr:arr,
-        listExists: true,});
-}); */
-
 // API
 router.get('/', async (req, res) => {
     // Agregando límite, si no se agrega el límite trae todo los productos, de traer el límite trae la cantidad indicada.
@@ -112,9 +19,7 @@ router.get('/', async (req, res) => {
     let sort = parseInt(req.query.sort) || '';
     const products = await Products.findAll(customQuery,page,limitValue,sort);
     res.json({messages: products});
-}) 
-
-
+})
 
 router.get('/:pid', async (req, res) => {
     const pid = req.params.pid;
@@ -141,9 +46,6 @@ router.delete('/:pid', async (req, res) => {
     const prod = await Products.deleteById(itemId);
     res.json({message:prod});
 });
-
-
-
 
 
 module.exports = router;
